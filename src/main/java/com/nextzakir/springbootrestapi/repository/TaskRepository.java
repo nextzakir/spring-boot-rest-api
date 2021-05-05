7024b35331d4e2f1afc7ca9c6c60ffc313c9362e
Zakir Hussain <zhzakir95@gmail.com>
2021-05-06 01:41:24 +0600

    Add repository annotation.

--- src/main/java/com/nextzakir/springbootrestapi/repository/TaskRepository.java
+++ src/main/java/com/nextzakir/springbootrestapi/repository/TaskRepository.java
@@ -3,9 +3,11 @@ package com.nextzakir.springbootrestapi.repository;
 import com.nextzakir.springbootrestapi.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     @Query(value = "SELECT * FROM tasks WHERE title = ?1", nativeQuery = true)
